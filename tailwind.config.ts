8229d781bb4583931852a9e6f5507d2e95ee07ef
sebastianzunigas <sebastian.hernanzuniga@gmail.com>
2026-08-25 17:59:30 -0400

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        slate: {
+          850: "#151e2e",
+          950: "#0b0f17",
+        }
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
